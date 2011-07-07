d0ee0a29bdcd081227acb8516a74722ae4c85f3b
Maxim Verzakov <m2xver@gmail.com>
2011-07-07 16:23:42 +0700

    add iterator class

--- class4.py
+++ class4.py
@@ -1,32 +1,26 @@
-class Reg():
-    reglist = []
-    def __init__(self):
-        Reg.reglist.append(self.__class__) 
-        Reg.offset = 0
-               
-    def __rerp__(self):
-        return self
-        
-    def __iter__(self):
-        return self
-         
-    def next(self):
-        if self.offset >= len(Reg.reglist):
-            raise StopIteration
-        result = Reg.reglist[self.offset]
-        self.offset += 1
-        return result
-           
-    def __del__(self):
-        Reg.reglist.remove(self.__class__)
-            
-            
-if __name__ == '__main__':
-    x = Reg()
-    y = Reg()
-    print x, y
-    print Reg.reglist
-    y = 2
-    print Reg.reglist
-    for i in Reg():
-        print i
+class IterableRegisterMetaClass(type):
+
+    def __init__(cls, name, bases, dictionary):
+
+        def counter(instance):
+            instance.__class__._instances.append(instance)
+
+        def decounter(instance):
+            instance.__class__._instances.remove(instance)
+
+        setattr(cls, '__init__', counter)
+        setattr(cls, '__del__', decounter)
+        setattr(cls, '_instances', [])
+
+    def __iter__(cls):
+        return iter(cls._instances)
+
+
+class Reg:
+    __metaclass__ = IterableRegisterMetaClass
+    
+x = Reg()
+y = Reg()
+z = Reg()
+for i in Reg:
+    print i
